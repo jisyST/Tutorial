0362a8f6608ba3bc24c9ad6d41e64906e3f6e5f6
wangjian <wangjian2@sensetime.com>
2025-04-07 11:41:47 +0800

    replace magic-pdf version

--- rag/codes/chapter5/reader_insufficient.py
+++ rag/codes/chapter5/reader_insufficient.py
@@ -2,6 +2,7 @@ from lazyllm.tools.rag import Document
 
 doc = Document(dataset_path="/mnt/lustre/share_data/dist/cmrc2018/data_kb")
 
-data = doc._impl._reader.load_data(input_files=["Masked Autoencoders Are Scalable Vision Learners.pdf"])
+data = doc._impl._reader.load_data(input_files=["平安证券-珀莱雅.pdf"])
 print(f"data: {data}")
 print(f"text: {data[0].text}")
+print(f"text: {data[1].text}")
