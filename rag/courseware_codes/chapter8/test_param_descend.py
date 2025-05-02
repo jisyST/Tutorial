9add927bc7c243c25e8102e190412fbd872c4488
JisyST <jisiyuan@sensetime.com>
2025-05-02 13:25:33 +0800

    modify chapter16

--- rag/courseware_codes/chapter8/test_param_descend.py
+++ rag/courseware_codes/chapter8/test_param_descend.py
@@ -2,8 +2,7 @@ import lazyllm
 import os
 import os
 import os, sys
-curt_file_path = os.path.realpath(__file__) if "__file__" in globals() else os.getcwd()
-sys.path = ["/home/mnt/jisiyuan/projects/LazyLLM"] + sys.path
+
 from lazyllm import OnlineEmbeddingModule, SentenceSplitter, Retriever
 from lazyllm.tools.rag import Document
 
